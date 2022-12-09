22e0e47ab67744f7947c8e3106d7749a952dc4fe
vothanhtinh <vothanhtinh147@gmail.com>
2022-12-09 13:08:06 +0700

    update

--- saleapp/index.py
+++ saleapp/index.py
@@ -6,5 +6,17 @@ from flask import render_template
 def home():
     return render_template("index.html")
 
+
+@app.route("/introduce")
+def introduce():
+    return render_template("introduce.html")
+@app.route("/booking")
+def booking():
+    return render_template("booking.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__=="__main__":
     app.run(debug=True)
